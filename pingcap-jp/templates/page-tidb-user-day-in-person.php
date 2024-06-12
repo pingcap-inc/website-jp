710c0636a6e77a1d1875d2a64a2d041cbfe607ff
xuliwenwenwen <liwen.xu@pingcap.com>
2024-06-12 11:52:06 +0800

    feat: update campaign image

--- pingcap-jp/templates/page-tidb-user-day-in-person.php
+++ pingcap-jp/templates/page-tidb-user-day-in-person.php
@@ -88,7 +88,13 @@ get_header();
                                         <dl>
                                             <dt>参加費</dt>
                                             <dd>
-                                                <p>無料　※完全招待制<br>名刺2枚</p>
+                                                <p>無料　※完全招待制</p>
+                                            </dd>
+                                        </dl>
+                                        <dl>
+                                            <dt>持ち物</dt>
+                                            <dd>
+                                                <p>名刺2枚</p>
                                             </dd>
                                         </dl>
                                     </div>
@@ -857,7 +863,7 @@ get_header();
                         <h2 class="a-heading ">
                             <span class="a-heading_text-one  tw-text-center tw-text-[3rem] md:tw-text-[4.2rem] tw-leading-normal">登録キャンペーン</span>
                         </h2>
-                        <p class="tw-text-[1.6rem] tw-mt-[6rem]">イベントに登録すると、抽選で100名様にTiDBのオリジナルTシャツ、または、3万円相当のTiDB資格試験「<a href="https://pingcap.co.jp/education/certification/pingcap-certified-tidb-associate/" class="a-text-link" target="_blank">PingCAP Certified TiDB Associate (PCTA)</a>」の無料受講パス (お申込日から366日間有効) をプレゼントいたします。イベント当日、アンケートに回答してご希望のプレゼントをご選択の上ご応募ください。<br>※PCTAは「TiDB基礎 (TiDB Essentials)」の内容を元に考案された試験内容となっており、TiDBの基本的な操作や概念について学ぶことができます。</p>
+                        <p class="tw-text-[1.6rem] tw-mt-[6rem]">イベントに登録すると、抽選で100名様にTiDBのオリジナルTシャツ、または、3万円相当のTiDB資格試験<span class="whitespace-nowrap">「<a href="https://pingcap.co.jp/education/certification/pingcap-certified-tidb-associate/" class="a-text-link" target="_blank">PingCAP Certified TiDB Associate (PCTA)</a>」</span>の無料受講パス (お申込日から366日間有効) をプレゼントいたします。イベント当日、アンケートに回答してご希望のプレゼントをご選択の上ご応募ください。<br>※PCTAは「TiDB基礎 (TiDB Essentials)」の内容を元に考案された試験内容となっており、TiDBの基本的な操作や概念について学ぶことができます。</p>
                     </div>
                 </div>
                 <div class="l-content is-w-1172 tw-mt-8 md:tw-mt-14">
