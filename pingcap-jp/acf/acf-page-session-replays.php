dd317740a26c8fd5b4815b90a6e3b860649ae6c7
xuliwenwenwen <74528885+xuliwenwenwen@users.noreply.github.com>
2024-07-26 13:29:41 +0800

    Feat/session replay (#22)
    * feat: session replays page
    
    * feat: video pdf download

---
+++ pingcap-jp/acf/acf-page-session-replays.php
@@ -0,0 +1,166 @@
+<?php
+$acf_group = 'tmpl_session_replay';
+
+acf_add_local_field_group(array(
+    'key' => 'group_' . $acf_group,
+    'title' => 'Session Replays Content',
+    'fields' => array(
+        array(
+            'key' => 'field_' . $acf_group . '_content',
+            'label' => 'Sections',
+            'name' => 'session_replay_content',
+            'type' => 'repeater',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'collapsed' => 'field_' . $acf_group . '_title',
+            'min' => 0,
+            'max' => '',
+            'layout' => 'block',         // table | block | row
+            'button_label' => 'Add Section',
+            'sub_fields' => array(
+                array(
+                    'key' => 'field_' . $acf_group . '_title',
+                    'label' => 'Title',
+                    'name' => 'title',
+                    'type' => 'text',
+                    'instructions' => '',
+                    'required' => 1,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'formatting' => 'none', // none | html
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                    'readonly' => 0,
+                    'disabled' => 0,
+                ),
+                array(
+                    'key' => 'field_' . $acf_group . '_video',
+                    'label' => 'Video',
+                    'name' => 'video',
+                    'type' => 'repeater',
+                    'instructions' => '',
+                    'required' => 1,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'collapsed' => '',
+                    'min' => 0,
+                    'max' => '',
+                    'layout' => 'block',         // table | block | row
+                    'button_label' => 'Add video',
+                    'sub_fields' => array_merge(
+                        array(
+                            array(
+                                'key' => 'field_' . $acf_group . '_video_image',
+                                'label' => 'Image',
+                                'name' => 'image',
+                                'instructions' => '',
+                                'type' => 'image',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'return_format' => 'object',       // array | url | id
+                                'preview_size' => 'thumbnail',
+                                'library' => 'all',       // all | uploadedTo
+                                'min_width' => '',
+                                'min_height' => '',
+                                'min_size' => '',
+                                'max_width' => '',
+                                'max_height' => '',
+                                'max_size' => '',
+                                'mime_types' => '',
+                            ),
+                            array(
+                                'key' => 'field_' . $acf_group . '_video_url',
+                                'label' => 'Url',
+                                'name' => 'url',
+                                'type' => 'text',
+                                'instructions' => '',
+                                'required' => 0,
+                                'conditional_logic' => 0,
+                                'wrapper' => array(
+                                    'width' => '',
+                                    'class' => '',
+                                    'id' => '',
+                                ),
+                                'default_value' => '',
+                                'placeholder' => '',
+                                'formatting' => 'none', // none | html
+                                'prepend' => '',
+                                'append' => '',
+                                'maxlength' => '',
+                                'readonly' => 0,
+                                'disabled' => 0,
+                            ),
+                        ),
+                        WPUtil\Vendor\BlueprintBlocks::safe_get_link_fields([
+                            'label' => 'Download PDF Button',
+                            'name' => 'video_pdf',
+                            'includes' => [
+                                'file' => 'File Download',
+                                'url' => 'URL',
+                                'page' => 'Page Link',
+                            ],
+                            'supports_button_styles' => false,
+                        ]),
+                    )
+                ),
+
+            ),
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'page_template', // post_type | post | page | page_template | post_category | taxonomy | options_page
+                'operator' => '==',
+                'value' => 'templates/page-session-replay.php',      // if options_page then use: acf-options  | if page_template then use:  template-example.php
+                'order_no' => 0,
+                'group_no' => 1,
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal', // side | normal | acf_after_title
+    'style' => 'default', // default | seamless
+    'label_placement' => 'top', // top | left
+    'instruction_placement' => 'label', // label | field
+    'hide_on_screen' => array(
+        // 0 => 'permalink',
+        // 1 => 'the_content',
+        // 2 => 'excerpt',
+        // 3 => 'custom_fields',
+        // 4 => 'discussion',
+        // 5 => 'comments',
+        // 6 => 'revisions',
+        // 7 => 'slug',
+        // 8 => 'author',
+        // 9 => 'format',
+        // 10 => 'featured_image',
+        // 11 => 'categories',
+        // 12 => 'tags',
+        // 13 => 'send-trackbacks'
+    ),
+    'active' => 1,
+    'description' => ''
+));
---
+++ pingcap-jp/templates/page-session-replay.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Template Name: 2024 session replay
+ */
+
+use WPUtil\{Arrays, Vendor};
+use WPUtil\Vendor\{ACF, BlueprintBlocks};
+use Blueprint\Images;
+
+get_header();
+
+?>
+<main class="tmpl-session-replay__content">
+    <div class="contain">
+        <h1 class="tmpl-session-replay__content-title">Session Replays</h1>
+        <?php
+        $replay_sections = ACF::get_field_array('session_replay_content');
+        foreach ($replay_sections as $index => $replay_section) {
+            $videos = Arrays::get_value_as_array($replay_section, 'video');
+            $title = Arrays::get_value_as_string($replay_section, 'title');
+        ?>
+            <div class="tmpl-session-replay__card">
+                <h5 class="tmpl-session-replay__card-title"><?php echo $title; ?></h5>
+                <div class="tmpl-session-replay__card-items">
+                    <?php
+                    foreach ($videos as $video) {
+                        $video_image = Arrays::get_value_as_array($video, 'image');
+                        $video_url = Arrays::get_value_as_string($video, 'url');
+                        $video_pdf = BlueprintBlocks::get_button_field_values('video_pdf', $video);
+                    ?>
+                        <div class="block-columns__column wysiwyg">
+                            <a class="block-columns__video-container js--trigger-video-modal ignore-link-styles" href="<?php echo esc_url($video_url); ?>">
+                                <?php
+                                Images::safe_image_output($video_image, ['class' => 'block-columns__video-image']);
+
+                                do_action('grav_blocks_get_video_link_button', '');
+                                ?>
+                            </a>
+                            <?php if ($video_pdf) { ?>
+                                <div class="text-right">
+                                    <a class="button" href="<?php echo $video_pdf->link; ?>"><?php echo $video_pdf->text; ?></a>
+                                </div>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</main>
+
+
+<?php
+
+Vendor\BlueprintBlocks::safe_display();
+
+get_footer();
