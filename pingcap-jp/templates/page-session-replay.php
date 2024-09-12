728774368e08b1cdf4723c9ddbd1a3778c08ddfa
xuliwenwenwen <liwen.xu@pingcap.com>
2024-09-12 17:20:01 +0800

    feat: remove play icon when no link

--- pingcap-jp/templates/page-session-replay.php
+++ pingcap-jp/templates/page-session-replay.php
@@ -30,13 +30,21 @@ get_header();
                         $video_pdf = BlueprintBlocks::get_button_field_values('video_pdf', $video);
                     ?>
                         <div class="block-columns__column wysiwyg">
-                            <a class="block-columns__video-container js--trigger-video-modal ignore-link-styles" href="<?php echo esc_url($video_url); ?>">
-                                <?php
-                                Images::safe_image_output($video_image, ['class' => 'block-columns__video-image']);
+                            <?php if ($video_url) { ?>
+                                <a class="block-columns__video-container js--trigger-video-modal ignore-link-styles" href="<?php echo esc_url($video_url); ?>">
+                                    <?php
+                                    Images::safe_image_output($video_image, ['class' => 'block-columns__video-image']);
 
-                                do_action('grav_blocks_get_video_link_button', '');
-                                ?>
-                            </a>
+                                    do_action('grav_blocks_get_video_link_button', '');
+                                    ?>
+                                </a>
+                            <?php } else { ?>
+                                <div class="block-columns__video-container">
+                                    <?php
+                                    Images::safe_image_output($video_image, ['class' => 'block-columns__video-image']);
+                                    ?>
+                                </div>
+                            <?php } ?>
                             <?php if ($video_pdf) { ?>
                                 <div class="text-right">
                                     <a class="button" href="<?php echo $video_pdf->link; ?>"><?php echo $video_pdf->text; ?></a>
