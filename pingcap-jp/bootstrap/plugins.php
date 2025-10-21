a2a3f420650c5defa8caa6b5e16c54c493a5720f
xuliwenwenwen <liwen.xu@pingcap.com>
2025-10-21 14:51:57 +0800

    feat: exclude noindex pages from sitemap

--- pingcap-jp/bootstrap/plugins.php
+++ pingcap-jp/bootstrap/plugins.php
@@ -14,6 +14,19 @@ add_filter('wpseo_metabox_prio', function () {
 	return 'low';
 });
 
+// Exclude noindex pages from Yoast sitemap
+add_filter('wpseo_exclude_from_sitemap_by_post_ids', function ($excluded_posts) {
+	
+	$unlisted_post_ids = get_option("unlist_posts", array());
+
+	if (!empty($unlisted_post_ids)) {
+		$excluded_posts = array_merge($excluded_posts, $unlisted_post_ids);
+	}
+
+	return $excluded_posts;
+});
+
+
 // ********************
 // Gravity Forms
 // ********************
