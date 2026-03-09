ea77922859d3f16823c741dda5f3a40d9392477a
xuliwenwenwen <liwen.xu@pingcap.com>
2026-03-09 12:58:25 +0800

    .

---
+++ pingcap-jp/acf/acf-page-tidb-user-day-2026.php
@@ -0,0 +1,496 @@
+<?php
+$acf_group = 'tmpl_page_tidb_user_day_2026';
+
+acf_add_local_field_group(array(
+	'key' => 'group_' . $acf_group,
+	'title' => 'TiDB User Day 2026 Content Settings',
+	'fields' => array(
+		/**
+		 * Tab: Banner
+		 */
+		array(
+			'key' => 'field_' . $acf_group . '_tab_banner',
+			'label' => 'Banner',
+			'name' => 'tab_banner',
+			'type' => 'tab',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'placement' => 'top',
+			'endpoint' => 0,          // end tabs to start a new group
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_banner_content',
+			'label' => 'Content',
+			'name' => 'banner_content',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'text',         // all | visual | text
+			'toolbar' => 'full',     // full | basic
+			'media_upload' => 0,
+		),
+		/**
+		 * Tab: Carousel
+		 */
+		array(
+			'key' => 'field_' . $acf_group . '_tab_carousel',
+			'label' => 'Carousel',
+			'name' => 'tab_carousel',
+			'type' => 'tab',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'placement' => 'top',
+			'endpoint' => 0,          // end tabs to start a new group
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_carousel_content',
+			'label' => 'Content',
+			'name' => 'carousel_content',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'text',         // all | visual | text
+			'toolbar' => 'full',     // full | basic
+			'media_upload' => 0,
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_carousel_list',
+			'label' => 'Carousel image',
+			'name' => 'carousel_list',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => '',
+			'max' => '',
+			'layout' => 'block',         // table | block | row
+			'button_label' => 'Add Image',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_' . $acf_group . '_carousel_image',
+					'label' => 'Image',
+					'name' => 'carousel_image',
+					'instructions' => '',
+					'type' => 'image',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'url',       // array | url | id
+					'preview_size' => 'medium',
+					'library' => 'all',       // all | uploadedTo
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+			),
+		),
+
+		/**
+		 * Tab: About
+		 */
+		array(
+			'key' => 'field_' . $acf_group . '_tab_about',
+			'label' => 'About',
+			'name' => 'tab_about',
+			'type' => 'tab',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'placement' => 'top',
+			'endpoint' => 0,          // end tabs to start a new group
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_about_content',
+			'label' => 'Content',
+			'name' => 'about_content',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'text',         // all | visual | text
+			'toolbar' => 'full',     // full | basic
+			'media_upload' => 0,
+		),
+
+		/**
+		 * Tab: Agenda
+		 */
+		array(
+			'key' => 'field_' . $acf_group . '_tab_agenda',
+			'label' => 'Agenda',
+			'name' => 'tab_agenda',
+			'type' => 'tab',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'placement' => 'top',
+			'endpoint' => 0,          // end tabs to start a new group
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_agenda_block_title',
+			'label' => 'Block Title',
+			'name' => 'agenda_block_title',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'formatting' => 'none',       // none | html
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+			'readonly' => 0,
+			'disabled' => 0,
+		),
+		array(
+			'key' => 'field_' . $acf_group . '_agenda_list',
+			'label' => 'Agenda',
+			'name' => 'agenda_list',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'collapsed' => '',
+			'min' => 0,
+			'max' => '',
+			'layout' => 'block',         // table | block | row
+			'button_label' => 'Add Agenda',
+			'sub_fields' => array(
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_card_color',
+					'label' => 'Card Color',
+					'name' => 'agenda_card_color',
+					'type' => 'select',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'choices' => array(
+						'red' => 'Red',
+						'green' => 'Green',
+						'blue' => 'Blue',
+						'violet' => 'Violet'
+					),
+					'default_value' => 'red',
+					'allow_null' => 0,
+					'multiple' => 0,         // allows for multi-select
+					'ui' => 0,               // creates a more stylized UI
+					'ajax' => 0,
+					'placeholder' => '',
+					'disabled' => 0,
+					'readonly' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_start_time',
+					'label' => 'Start Time',
+					'name' => 'agenda_start_time',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '50',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'formatting' => 'none',       // none | html
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_end_time',
+					'label' => 'End Time',
+					'name' => 'agenda_end_time',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '50',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'formatting' => 'none',       // none | html
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_image',
+					'label' => 'Avatar',
+					'name' => 'agenda_image',
+					'instructions' => '',
+					'type' => 'image',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'return_format' => 'array',       // array | url | id
+					'preview_size' => 'thumbnail',
+					'library' => 'all',       // all | uploadedTo
+					'min_width' => '',
+					'min_height' => '',
+					'min_size' => '',
+					'max_width' => '',
+					'max_height' => '',
+					'max_size' => '',
+					'mime_types' => '',
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_has_multiple_avatar',
+					'label' => 'Has Multiple Avatars',
+					'name' => 'has_multiple_avatar',
+					'type' => 'true_false',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'message' => '',
+					'ui' => 1,
+					'ui_on_text' => 'Yes',
+					'ui_off_text' => 'No',
+					'default_value' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_multiple_avatar',
+					'label' => 'Avatars',
+					'name' => 'multiple_avatar',
+					'type' => 'repeater',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => array(
+						array(
+							array(
+								'field' => 'field_' . $acf_group . '_has_multiple_avatar',
+								'operator' => '==',
+								'value' => 1,
+							),
+						),
+					),
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'collapsed' => '',
+					'min' => 0,
+					'max' => '',
+					'layout' => 'block',         // table | block | row
+					'button_label' => 'Add Avatar',
+					'sub_fields' => array(
+						array(
+							'key' => 'field_' . $acf_group . '_avatar',
+							'label' => 'Avatar',
+							'name' => 'avatar',
+							'instructions' => '',
+							'type' => 'image',
+							'required' => 0,
+							'conditional_logic' => 0,
+							'wrapper' => array(
+								'width' => '',
+								'class' => '',
+								'id' => '',
+							),
+							'return_format' => 'array',       // array | url | id
+							'preview_size' => 'thumbnail',
+							'library' => 'all',       // all | uploadedTo
+							'min_width' => '',
+							'min_height' => '',
+							'min_size' => '',
+							'max_width' => '',
+							'max_height' => '',
+							'max_size' => '',
+							'mime_types' => '',
+						),
+					)
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_title',
+					'label' => 'Title',
+					'name' => 'agenda_title',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 1,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'formatting' => 'none',       // none | html
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_desc',
+					'label' => 'Desc',
+					'name' => 'agenda_desc',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'formatting' => 'none',       // none | html
+					'prepend' => '',
+					'append' => '',
+					'maxlength' => '',
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+				array(
+					'key' => 'field_' . $acf_group . '_agenda_summary',
+					'label' => 'Summary',
+					'name' => 'agenda_summary',
+					'type' => 'textarea',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'placeholder' => '',
+					'maxlength' => '',
+					'rows' => 4,
+					'new_lines' => "\n",        // wpautop | br | ''
+					'readonly' => 0,
+					'disabled' => 0,
+				),
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'page_template', // post_type | post | page | page_template | post_category | taxonomy | options_page
+				'operator' => '==',
+				'value' => 'templates/page-tidb-user-day-2026.php',      // if options_page then use: acf-options  | if page_template then use:  template-example.php
+				'order_no' => 0,
+				'group_no' => 1,
+			),
+		)
+	),
+	'menu_order' => 0,
+	'position' => 'normal', // side | normal | acf_after_title
+	'style' => 'default', // default | seamless
+	'label_placement' => 'top', // top | left
+	'instruction_placement' => 'label', // label | field
+	'hide_on_screen' => array(
+		// 0 => 'permalink',
+		// 1 => 'the_content',
+		// 2 => 'excerpt',
+		// 3 => 'custom_fields',
+		// 4 => 'discussion',
+		// 5 => 'comments',
+		// 6 => 'revisions',
+		// 7 => 'slug',
+		// 8 => 'author',
+		// 9 => 'format',
+		// 10 => 'featured_image',
+		// 11 => 'categories',
+		// 12 => 'tags',
+		// 13 => 'send-trackbacks'
+	),
+	'active' => 1,
+	'description' => ''
+));
---
+++ pingcap-jp/templates/page-tidb-user-day-2026.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Template Name: TiDB User Day 2026
+ */
+
+use Blueprint\Images;
+use WPUtil\{Arrays, Vendor};
+use WPUtil\Vendor\ACF;
+
+get_header();
+
+?>
+<div class="tmpl-tidb-user-day-2026">
+    <div class="banner">
+        <div class="inner">
+            <?php echo ACF::get_field_string('banner_content'); ?>
+        </div>
+    </div>
+    <section class="block-options-padding-remove-top introduction bg-black-dark block-container block-columns" aria-label="Columns">
+        <div class="block-inner contain grid is-12" data-num-col="1" data-format="">
+            <div class="block-columns__column wysiwyg">
+                <?php echo ACF::get_field_string('carousel_content'); ?>
+            </div>
+        </div>
+    </section>
+
+    <?php
+    $image_list = ACF::get_field_array('carousel_list');
+    if (count($image_list) > 0) {
+    ?>
+        <section class="carousel block-options-padding-remove-top bg-black-dark">
+            <div class="carousel-list-container">
+                <div class="carousel-list">
+                    <?php
+                    foreach ($image_list as $image) {
+                        echo '<div class="carousel-item">';
+                        echo '<img src="' . Arrays::get_value_as_string($image, 'carousel_image') . '" class="carousel-item-image">';
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
+                <div class="carousel-list">
+                    <?php
+                    $image_list = ACF::get_field_array('carousel_list');
+                    foreach ($image_list as $image) {
+                        echo '<div class="carousel-item">';
+                        echo '<img src="' . Arrays::get_value_as_string($image, 'carousel_image') . '" class="carousel-item-image">';
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </section>
+    <?php } ?>
+
+    <section id="about" class="about bg-black-gradient block-container">
+        <div class="block-inner contain">
+            <?php echo ACF::get_field_string('about_content'); ?>
+        </div>
+    </section>
+
+    <?php
+    $agenda_list = ACF::get_field_array('agenda_list');
+    if (count($agenda_list)) {
+    ?>
+        <section id="agenda" class="agenda bg-black-dark block-container">
+            <div class="block-inner contain">
+                <div class="block-title">
+                    <h4><?php echo ACF::get_field_string('agenda_block_title'); ?></h4>
+                </div>
+                <div class="agenda-list">
+                    <?php
+                    foreach ($agenda_list as $list) {
+                        $agenda_card_color = Arrays::get_value_as_string($list, 'agenda_card_color');
+                        $agenda_start_time = Arrays::get_value_as_string($list, 'agenda_start_time');
+                        $agenda_end_time = Arrays::get_value_as_string($list, 'agenda_end_time');
+                        $agenda_image = Arrays::get_value_as_array($list, 'agenda_image');
+                        $agenda_title = Arrays::get_value_as_string($list, 'agenda_title');
+                        $agenda_desc = Arrays::get_value_as_string($list, 'agenda_desc');
+                        $agenda_summary = Arrays::get_value_as_string($list, 'agenda_summary');
+                        $has_multiple_avatar = Arrays::get_value_as_bool($list, 'has_multiple_avatar');
+                        $multiple_avatar = Arrays::get_value_as_array($list, 'multiple_avatar');
+                    ?>
+                        <div class="timeline <?php echo $agenda_summary ? 'timeline--has-summary js--trigger-tiud-summary-modal' : ''; ?>">
+                            <div class="time"><?php echo $agenda_start_time; ?><span class="line"></span><?php echo $agenda_end_time; ?></div>
+                            <div class="card <?php echo $agenda_card_color ?> <?php echo !$agenda_image ? 'bg-' . $agenda_card_color : ''; ?>">
+                                <div>
+                                    <div class="image-container <?php echo $agenda_image ? 'has-image' : ''; ?> <?php echo $has_multiple_avatar ? 'has-multiple' : ''; ?>">
+                                        <?php Images::safe_image_output($agenda_image, ['data-lazy-ignore' => 1]); ?>
+                                    </div>
+                                    <?php if ($has_multiple_avatar) { ?>
+                                        <div class="avatars">
+                                            <?php foreach ($multiple_avatar as $avatar) {
+                                                Images::safe_image_output(Arrays::get_value_as_array($avatar, 'avatar'), ['data-lazy-ignore' => 1]);
+                                            }  ?>
+                                        </div>
+                                    <?php } ?>
+                                </div>
+                                <div class="content">
+                                    <h3><?php echo $agenda_title; ?></h3>
+                                    <?php if ($agenda_desc) { ?>
+                                        <p><?php echo $agenda_desc; ?></p>
+                                    <?php } ?>
+                                    <?php if ($agenda_summary) { ?>
+                                        <div class="summary"><?php echo $agenda_summary; ?></div>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    }
+                    ?>
+
+                </div>
+            </div>
+        </section>
+    <?php
+    }
+    ?>
+
+    <?php echo Vendor\BlueprintBlocks::safe_display(); ?>
+</div>
+
+<script>
+    const navbarEl = document.querySelector('.navbar-toggle');
+    const navEl = document.querySelector('.tmpl-tidb-user-day-2025__header nav');
+    const navMenuEls = document.querySelectorAll('.tmpl-tidb-user-day-2025__header .nav-menu');
+    navbarEl.addEventListener('click', () => {
+        if (navEl.classList.contains('active')) {
+            navEl.classList.remove('active');
+        } else {
+            navEl.classList.add('active');
+        }
+    });
+    navMenuEls.forEach(el => {
+        el.addEventListener('click', () => {
+            navEl.classList.remove('active');
+        })
+    });
+</script>
+
+<?php
+
+get_footer();
